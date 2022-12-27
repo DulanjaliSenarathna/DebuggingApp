8bd0f712ce5146a77330a69bd63d071d1b9631e5
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 10:18:58 +0530

    Removing Side Effects

--- DebuggingApp/Program.cs
+++ DebuggingApp/Program.cs
@@ -10,7 +10,7 @@ namespace DebuggingApp
             var numbers = new List<int>() {1,2,3,4,5,6 };
             var smallest = GetSmallests(numbers, 3);
 
-            foreach(var number in smallest)
+            foreach(var number in smallest)//if we iterate over original list(numbers), we can get output 4,5,6. it means list remove first 3 numbers. this is a side effect.
             {
                 Console.WriteLine(number);
             }
@@ -18,13 +18,14 @@ namespace DebuggingApp
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            var buffer = new List<int>(list);//create a copy of the list and do processing by it.
             var smallests = new List<int>();
 
             while (smallests.Count < count)
             {
-                var min = GetSmallest(list);
+                var min = GetSmallest(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);//now check original list, it contains all 6 numbers.
             }
 
             return smallests;
