d3d526b6de14fb6517260d0bd8453d4ef3309e65
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-27 08:40:05 +0530

    Initial commit

---
+++ DebuggingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DebuggingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
